ba9fb5b1093af7e1bd6d57f9aef4610e13c16365
dothuong <dothuong2k1@gmail.com>
2023-12-13 21:48:51 +0700

    change firebaseConfig

--- src/firebase.js
+++ src/firebase.js
@@ -7,16 +7,14 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBZ5K_u9bVYlSZUXbq0WUBKwojw-vlrBVo",
-  authDomain: "social-media-1-a4d1e.firebaseapp.com",
-  projectId: "social-media-1-a4d1e",
-  storageBucket: "social-media-1-a4d1e.appspot.com",
-  messagingSenderId: "585114795929",
-  appId: "1:585114795929:web:73e607188a195946b7dd08",
-  measurementId: "G-3BP6TH78MQ"
+  apiKey: "AIzaSyD9kYr4xIhhX8bmGV93qKY6P1d6m35Fk5Q",
+  authDomain: "social-media-2-e01b4.firebaseapp.com",
+  projectId: "social-media-2-e01b4",
+  storageBucket: "social-media-2-e01b4.appspot.com",
+  messagingSenderId: "175139590860",
+  appId: "1:175139590860:web:579cd8895706ac1cce6869",
+  measurementId: "G-4NGF00589Y"
 };
-
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
